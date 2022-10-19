ed405f64a3b5a8725903236a09ab376726dfeb0a
Hitesh mishra <mishrahitesh854@gmail.com>
2022-10-19 09:56:53 +0530

    added staticmember.cpp learned what are static data members and static functions

--- oops/staticdatamember.cpp
+++ oops/staticdatamember.cpp
@@ -3,16 +3,37 @@ using namespace std;
 
 class Employee{
     int id;
-    int count;
+    static int count;//static variable are initialized to 0 by default. 
     public:
         void setdata(void){
+            cout<<"Enter the id:"<<endl;
             cin>>id;
+            count++;
         }
         void getdata(void){
-            cout<<id;
+            cout<<"The id of this employee is "<<id<<" and this is employee number: "<< count <<endl;
         }
-}
+        static void getcount(void){
+            cout<<"The value of count is "<<count<<endl;
+        }
+};
+
+int Employee :: count=1000; 
 
 int main(){
-    Employee harry
+    Employee harry, rohan, lovish;
+    harry.setdata();
+    harry.getdata();
+
+    Employee :: getcount();
+
+    rohan.setdata();
+    rohan.getdata();
+    Employee :: getcount();
+    
+    lovish.setdata();
+    lovish.getdata();
+    Employee :: getcount();
+
+    return 0;
 }
