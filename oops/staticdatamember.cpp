e055a44c5ccc191c46b011f4842efa4779c98fe8
Hitesh mishra <mishrahitesh854@gmail.com>
2022-10-19 09:17:18 +0530

    started learning oops learned about classes and objects in cpp

---
+++ oops/staticdatamember.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Employee{
+    int id;
+    int count;
+    public:
+        void setdata(void){
+            cin>>id;
+        }
+        void getdata(void){
+            cout<<id;
+        }
+}
+
+int main(){
+    Employee harry
+}
