8c7ae80e36a96abb556c1dc8c2d68bc2902d8dd7
Hitesh mishra <mishrahitesh854@gmail.com>
2023-01-05 11:51:20 +0530

    made some changes to accessmodifiers.cpp

--- oops/accessmodifiers.cpp
+++ oops/accessmodifiers.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 using namespace std;
 
-class employee{
-    private:
-        int a, b, c;
-    public:
-        int d, e;
-        void setData(a, b, c){
+class employee
+{
+private:
+    int a, b, c;
 
-        }
-        void getData(){
-            cout<<"Enter the value of a: ";
-            cin>>a;
-            cout<<"Enter the value of b: ";
-            cin>>b;
-            cout<<"Enter the value of b: ";
-            cin>>b;
-        }
+public:
+    int d, e;
+
+    void setData();
+
+    void getData()
+    {
+        cout << "Value of a is: " << a << endl;
+        cout << "Value of b is: " << b << endl;
+        cout << "Value of c is: " << c << endl;
+    }
+};
+
+void employee::setData()
+{
+    cout << "Enter the value of a: ";
+    cin >> a;
+    cout << "Enter the value of b: ";
+    cin >> b;
+    cout << "Enter the value of b: ";
+    cin >> c;
 }
 
-void main(){
-
+int main()
+{
+    employee e1;
+    e1.setData();
+    e1.getData();
+    return 0;
 }
